d512809e99967c826c2374dd93ae16aabd26e8a5
Žiga Vukčevič <ziga@be-codified.com>
2016-11-30 19:27:02 +0100

    Outputting basic html file with certain content

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(__dirname + '/content.html');
 });
 
 app.listen(3000, () => {
