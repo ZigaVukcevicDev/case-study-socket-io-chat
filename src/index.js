2d64aa32314e16d7f2ea2e2611378c3ad2e8ea21
Žiga Vukčevič <ziga@be-codified.com>
2016-11-30 19:13:33 +0100

    Test for outputting html element from server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { createServer } from 'http';
 
 createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello world\n')
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<input type="text" value="testing"/>');
+
 }).listen(3000, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:3000/');
